2d6c145a388a0b9b1b256503581cb632d9f8144d
pablo chesini <pchesini1@gmail.com>
2025-08-10 18:59:07 -0300

    Inicializa proyecto Spring Boot con configuración de Maven y Docker

---
+++ src/test/java/com/tp_5/ADR/ComputingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tp_5.ADR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComputingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
